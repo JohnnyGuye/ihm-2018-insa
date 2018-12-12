b66defeb01eb864d3a7b52c0b718f80a66171d95
Johnny Guye <johnnyq@hotmail.fr>
2018-12-12 22:43:23 +0100

    Rewrite EventAsso with a closure

--- IHM/JS/event-feed-component.js
+++ IHM/JS/event-feed-component.js
@@ -1,17 +1,17 @@
 Vue.component('feed-event', {
-    props: ['item'],
+    props: ['event'],
     template:
         `
         <v-card>
         <v-card-title primary-title>
             <div>
-                <h3 class="headline mb-0">{{item.m_name}}</h3>
-                <div>{{item.m_asso}}</div>
+                <h3 class="headline mb-0">{{event.name}}</h3>
+                <div>{{event.asso}}</div>
             </div>
         </v-card-title>
-        <v-card-media>
-            <v-img v-bind:src="item.m_image"></v-img>
-        </v-card-media>
+        <v-responsive>
+            <v-img v-bind:src="event.imageURI"></v-img>
+        </v-responsive>
     </v-card>
     `
 })
--- IHM/JS/eventAsso.js
+++ IHM/JS/eventAsso.js
@@ -1,66 +1,24 @@
-var EventAsso = function(dateTime, name, place, importance)
-{
+let EventAsso = function( name, date, place, asso, imageURI, importance ) {
 
-	this.m_importance = 0;
-	this.m_place = null;
-    this.m_name = null;
-    this.m_dateTime = null;
+	let _date = ( date instanceof Date ? date : new Date( date ) )
 
+	return {
 
-    this.importance(importance);
-    this.name(name);
-    this.place(place);
-    this.dateTime(dateTime);
+		importance: importance,
+		name: name,
+		place: place,
+		asso: asso,
+		imageURI: imageURI,
 
-};
+		get date() {
+			return _date
+		},
+		set date( value ) {
+			if( value instanceof Date )
+				_date = value
+			else
+				_date = new Date( value )
+		}
 
-EventAsso.prototype.importance = function(importance)
-{
-	if (typeof importance !== "undefined")
-	{
-		this.m_importance = parseInt(importance);
 	}
-
-	return this.m_importance;
-
-}
-
-EventAsso.prototype.name = function (name)
-{
-	if (typeof name !== "undefined")
-	{
-		this.m_name = name;
-	}
-
-	return this.m_name;
-}
-
-EventAsso.prototype.dateTime = function(dateTime)
-{
-	if (typeof dateTime === 'undefined')
-	{
-		return this.m_dateTime;
-	}
-	else if (typeof dateTime.getMonth === 'function')
-	{
-		this.m_dateTime = dateTime;
-	}
-	else if (typeof dateTime !== 'undefined')
-	{
-		this.m_dateTime = new Date(dateTime);
-	}
-
-	return this.m_dateTime;
-
-
-}
-
-EventAsso.prototype.place = function(place)
-{
-	if (typeof place !== "undefined")
-	{
-		this.m_place = place;
-	}
-
-	return this.m_place;
 }
