18c0c78835b47738459a619bed0833e4ca86fbb9
Johnny Guye <johnnyq@hotmail.fr>
2018-12-26 19:33:44 +0100

    Add all IHM

---
+++ IHM/JS/description-component.js
@@ -0,0 +1,34 @@
+Vue.component(
+  "asso-description",
+  {
+    template:
+    `
+<div>
+  <div class="row">
+    <div class="col" style="margin-left:10px;margin-right:10px;text-align: justify;">
+      <h4>Qui somme nous ?</h4>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam
+      </p>
+    </div>
+  </div>
+
+  <div class="row">
+    <div class="col" style="margin-left:10px;margin-right:10px; text-align: justify;">
+      <h4>Que faisons nous ?</h4>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam
+      </p>
+    </div>
+  </div>
+  <div class="row">
+    <div class="col" style="margin-left:10px;margin-right:10px; text-align: justify;">
+      <h4>Watcha come in ? :)</h4>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit
+      </p>
+    </div>
+  </div>
+</div>
+    `
+})
