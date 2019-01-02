a5cf50e2844e1a5f190e3ef6bbeec0caa4f89cdb
AntonainV <antonin.vianey@live.com>
2019-01-02 15:05:13 +0100

    Roll Month and Year

--- IHM/JS/calendar.js
+++ IHM/JS/calendar.js
@@ -12,15 +12,6 @@ var Calendar = function ()
 	this.m_monthPreview = this.m_todayDate.getMonth();
 	this.m_yearPreview = this.m_todayDate.getFullYear();
 
-	this.init = init;
-	this.display = display;
-	this.initBtnsDate = initBtnsDate;
-	this.translateMonth = translateMonth;
-	this.getImportanceEventsDay = getImportanceEventsDay;
-	this.chooseMonth=chooseMonth;
-
-	this.addEvent = addEvent;
-
 };
 
 Calendar.MONTHS = Object.freeze([
@@ -48,13 +39,13 @@ Calendar.DAYS = Object.freeze([
 	"Dimanche"
 ])
 
-function init()
+Calendar.prototype.init = function() 
 {
 	this.display();
 	this.initBtnsDate();
 }
 
-function addEvent(event)
+Calendar.prototype.addEvent = function(event) 
 {
 	var dateTime = event.date;
 	var date = dateTime.getDate();
@@ -73,7 +64,7 @@ function addEvent(event)
 
 }
 
-function display()
+Calendar.prototype.display = function() 
 {
 	var month =  this.m_monthPreview;
 	var year = this.m_yearPreview;
@@ -191,7 +182,7 @@ function display()
 
 }
 
-function initBtnsDate ()
+Calendar.prototype.initBtnsDate = function() 
 {
 	var calendar = this;
 
@@ -246,7 +237,7 @@ function initBtnsDate ()
 
 }
 
-function translateMonth(nbMonth)
+Calendar.prototype.translateMonth = function(nbMonth) 
 {
 	var month = this.m_monthPreview;
 	var year = this.m_yearPreview;
@@ -270,7 +261,19 @@ function translateMonth(nbMonth)
 
 }
 
-function getImportanceEventsDay(day, month, year)
+Calendar.prototype.translateYear = function(nbYear) 
+{
+
+	var newYear = this.m_yearPreview + nbYear;
+	this.m_yearPreview = newYear;
+
+	this.display();
+	this.initBtnsDate();
+
+}
+
+
+Calendar.prototype.getImportanceEventsDay = function(day, month, year)
 {
 	var string = day+"-"+month+"-"+year;
 
@@ -298,13 +301,21 @@ function getImportanceEventsDay(day, month, year)
 	return maxImportance;
 }
 
-function chooseMonth ()
+Calendar.prototype.chooseYear = function()
 {
 	var darkScreen = document.createElement("div");
 	darkScreen.className="darkScreen";
 
 	var frame = document.createElement("div");
-	frame.className="frame";
+	frame.className="frame roll";
+
+	darkScreen.addEventListener("click",function()
+	{
+		document.body.removeChild(this);
+		document.body.removeChild(frame);
+	});
+
+	
 
 	var select = document.createElement("div");
 	select.className="dateChooser";
@@ -315,9 +326,146 @@ function chooseMonth ()
 	var target = document.createElement("div");
 	target.className="targetChoice";
 
-	var linearGradient1 = document.createElement("div");
-	linearGradient1.className = "linearGardientTopToBottom";
-	frame.append(linearGradient1);
+	var choosenYear = this.m_monthPreview;
+
+	var options = [];
+	var currentYearOption;
+
+
+	var thisYear = this.m_todayDate.getFullYear();
+
+	var optionLastYear =  document.createElement("div");
+	optionLastYear.setAttribute("value", thisYear-1);
+	optionLastYear.textContent =  thisYear-1;
+	choice.append(optionLastYear);
+
+	optionLastYear.addEventListener("click", function()
+    {
+    	changeOptionRoll(select, this, target);
+
+    });
+
+    options.push(optionLastYear);
+
+	var optionThisYear =  document.createElement("div");
+	optionThisYear.setAttribute("value", thisYear);
+	optionThisYear.textContent =  thisYear;
+	choice.append(optionThisYear);
+
+	optionThisYear.addEventListener("click", function()
+    {
+    	changeOptionRoll(select, this, target);
+
+    });
+
+	options.push(optionThisYear);
+
+
+	var optionNextYear =  document.createElement("div");
+	optionNextYear.setAttribute("value", thisYear+1);
+	optionNextYear.textContent =  thisYear+1;
+	choice.append(optionNextYear);
+
+	optionNextYear.addEventListener("click", function()
+    {
+    	changeOptionRoll(select, this, target);
+
+    });
+
+    options.push(optionNextYear);
+
+	
+	var btnValidate = document.createElement("span");
+    btnValidate.className="btnV";
+    btnValidate.textContent="ok";
+
+    var calendar = this;
+    btnValidate.addEventListener("click", function()
+    {
+  
+    	calendar.translateYear(choosenYear - calendar.m_yearPreview);
+    	document.body.removeChild(darkScreen);
+    	document.body.removeChild(frame);
+    });
+
+    target.append(btnValidate);
+	 
+
+	
+	select.append(choice);
+	select.append(target);
+
+	frame.append(select);
+	var lastPosY;
+
+
+
+	var timeOut;
+	
+
+	frame.addEventListener('wheel', function(e) 
+	{
+		clearTimeout(timeOut);
+      	timeOut = setTimeout(function() 
+		{
+			var choosenOption = stabilizeRoll(select, options, target);
+			choosenYear = choosenOption.getAttribute("value");
+		}, 100);
+
+	});
+
+	
+
+	frame.addEventListener('scroll', function(e)
+	{
+		clearTimeout(timeOut);
+      	timeOut = setTimeout(function() 
+		{
+			var choosenOption = stabilizeRoll(select, options, target);
+			choosenYear = choosenOption.getAttribute("value");
+
+		}, 100);
+    }, true);
+
+   
+	
+	document.body.append(darkScreen);
+	document.body.append(frame);
+
+	
+	choosenYear = changeOptionRoll(select, optionThisYear, target);
+
+}
+
+Calendar.prototype.chooseMonth = function()
+{
+	var darkScreen = document.createElement("div");
+	darkScreen.className="darkScreen";
+
+	var frame = document.createElement("div");
+	frame.className="frame roll";
+
+	darkScreen.addEventListener("click",function()
+	{
+		document.body.removeChild(this);
+		document.body.removeChild(frame);
+	});
+
+	
+
+	var select = document.createElement("div");
+	select.className="dateChooser";
+
+	var choice = document.createElement("div");
+	choice.className="dateChoice";
+
+	var target = document.createElement("div");
+	target.className="targetChoice";
+
+	var choosenMonth = this.m_monthPreview;
+
+	var options = [];
+	var currentMonthOption;
 
 	Calendar.MONTHS.forEach(
 		(month, idx) => {
@@ -329,25 +477,125 @@ function chooseMonth ()
 			if (idx ==  this.m_monthPreview)
 			{
 				option.setAttribute("selected","selected");
+				
 			}
 			else
 			{
 				option.removeAttribute("selected");
 			}
+
+			if (idx ==  this.m_todayDate.getMonth())
+			{
+				currentMonthOption = option;
+				
+			}
+
+			option.setAttribute("value",idx);
 			choice.append(option);
+
+			option.addEventListener("click", function()
+		    {
+		    	changeOptionRoll(select, this, target);
+
+		    });
+
+
+			options.push(option);
 		}
 
 	)
 
-	var linearGradient2 = document.createElement("div");
-	linearGradient2.className = "linearGardientBottomToTop";
-	frame.append(linearGradient2);
-	choice.append(target);
+	var btnValidate = document.createElement("span");
+    btnValidate.className="btnV";
+    btnValidate.textContent="ok";
+
+    var calendar = this;
+    btnValidate.addEventListener("click", function()
+    {
+  
+    	calendar.translateMonth(choosenMonth - calendar.m_monthPreview);
+    	document.body.removeChild(darkScreen);
+    	document.body.removeChild(frame);
+    });
+
+    target.append(btnValidate);
+	 
+
+	
 	select.append(choice);
+	select.append(target);
 
 	frame.append(select);
+	var lastPosY;
 
 
+
+	var timeOut;
+	
+
+	frame.addEventListener('wheel', function(e) 
+	{
+		clearTimeout(timeOut);
+      	timeOut = setTimeout(function() 
+		{
+			var choosenOption = stabilizeRoll(select, options, target);
+			choosenMonth = choosenOption.getAttribute("value");
+		}, 100);
+
+	});
+
+	
+
+	frame.addEventListener('scroll', function(e)
+	{
+		clearTimeout(timeOut);
+      	timeOut = setTimeout(function() 
+		{
+			var choosenOption = stabilizeRoll(select, options, target);
+			choosenMonth = choosenOption.getAttribute("value");
+
+		}, 100);
+    }, true);
+
+   
+	
 	document.body.append(darkScreen);
 	document.body.append(frame);
+
+	
+	choosenMonth = changeOptionRoll(select, currentMonthOption, target);
+
+}
+
+
+function changeOptionRoll (select, option, target)
+{
+	var scrollTop = select.scrollTop;
+	var diff = option.offsetTop - scrollTop - target.offsetTop + 19;
+	select.scrollTop+=diff;
+	return option.getAttribute("value");
+}
+
+
+function stabilizeRoll(select, options, target)
+{
+	var scrollTop = select.scrollTop;
+	var min;
+	var first = true;
+	var optionChoosen;
+
+	for (var i = 0; i < options.length; i++)
+	{
+		var diff = options[i].offsetTop - scrollTop - target.offsetTop + 19;
+		
+		if (first || Math.abs(diff) < Math.abs(min))
+		{
+			min = diff;
+			first = false;
+			optionChoosen = options[i];
+		}
+	}
+
+	select.scrollTop+=min;
+	return optionChoosen
 }
