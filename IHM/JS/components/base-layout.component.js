0ec42e54b3f10395616af34fdd0256e696ad463e
AntonainV <antonin.vianey@live.com>
2019-01-03 23:48:19 +0100

    fix Signed-off-by: AntonainV <antonin.vianey@live.com>

--- IHM/JS/components/base-layout.component.js
+++ IHM/JS/components/base-layout.component.js
@@ -1,11 +1,42 @@
 Vue.component(
   'base-layout', {
     props: [],
+    mounted() {
+        var el = document.getElementsByClassName('content')[0];
+        var up = document.getElementsByClassName('up')[0];
+        el.addEventListener('wheel', function(e) 
+        {
+          if (this.scrollTop == 0)
+          {
+            up.style.opacity="0";
+          } 
+          else
+          {
+            up.style.opacity="1";
+          }
+
+        }); 
+
+        el.addEventListener('scroll', function(e)
+        {
+          if (this.scrollTop == 0)
+          {
+              up.style.opacity="0";
+          } 
+          else
+          {
+              up.style.opacity="1";
+          }
+         
+        }, true);
+    },
     template:
     `
     <div class="main-container">
     <header class="z-index-1 primary-bg">
+      <div class="fabButton addEvent"  onclick="addEvent()">+</div>
       <slot name="header"></slot>
+      <div class="fabButton up" onclick="up()">↟</div>
     </header>
     <main class="content">
       <slot></slot>
@@ -16,3 +47,14 @@ Vue.component(
     </div>
     `
 })
+
+
+function up()
+{
+
+  var up = document.getElementsByClassName('up')[0];
+  var el = document.getElementsByClassName('content')[0];
+  el.scrollTop = 0;
+  up.style.opacity="0";
+  
+}
