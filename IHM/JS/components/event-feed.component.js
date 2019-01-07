5feff80b945f5567de592b037840fa9943edf624
Antonin Vianey <avianey@insa-lyon.fr>
2019-01-07 11:50:15 +0100

    Signed-off-by: Antonin Vianey <avianey@insa-lyon.fr>

--- IHM/JS/components/event-feed.component.js
+++ IHM/JS/components/event-feed.component.js
@@ -5,8 +5,16 @@ Vue.component('feed-event', {
     <v-card>
       <v-card-title primary-title>
         <div>
+          <div class="moreOption">...</div>
           <h3 class="headline mb-0">{{event.name}}</h3>
           <div>{{event.asso}}</div>
+          <ul class="moreOptionMenu">
+            <li class="followEvtOpt">Suivre l'événement</li>
+            <li class="HideEvtOpt">Masquer l'événement</li>
+            <li class="HideEvtAssoOpt">Masquer les événements de {{event.asso}}</li>
+            <li class="ShowDetEvt">Plus de détails</li>
+            <li class="ContAsso">Contacter l'organsiation</li>
+          </ul>
         </div>
       </v-card-title>
       <v-responsive>
