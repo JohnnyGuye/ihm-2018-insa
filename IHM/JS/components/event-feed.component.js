490bc0e6b1845759a64cbcc7896300974a2494b6
Antonin Vianey <avianey@insa-lyon.fr>
2019-01-07 16:09:09 +0100

    Signed-off-by: Antonin Vianey <avianey@insa-lyon.fr>

--- IHM/JS/components/event-feed.component.js
+++ IHM/JS/components/event-feed.component.js
@@ -1,14 +1,69 @@
 Vue.component('feed-event', {
     props: ['event'],
+    methods: {
+      openOption (event)
+      {
+        
+        var vue = this;
+
+        var moreOptionBtn = event.target;
+        var moreOptionMenu = moreOptionBtn.nextElementSibling;
+        var display = moreOptionMenu.style.display;
+        console.log(display);
+        if (display == "block")
+        {
+          moreOptionMenu.style.display = "none";
+
+          
+
+        }
+        else
+        {
+          console.log("2 " + display);
+
+          moreOptionMenu.style.display = "block";
+
+          moreOptionBtn.addEventListener("mousedown", function(event)
+          {
+             /*var moreOptionMenus = document.getElementsByClassName('moreOptionMenu');
+             for (var i = 0; i < moreOptionMenus; i++)
+             { 
+              moreOptionMenus[i].display="none";
+             } */
+
+            event.stopPropagation();
+          },
+          { 
+            once: true
+          });
+
+          moreOptionMenu.addEventListener("mousedown", function(event)
+          {
+            event.stopPropagation();
+          },
+          { 
+            once: true
+          });
+
+          document.body.addEventListener("mousedown", function()
+          {
+            moreOptionMenu.style.display="none";
+           },
+          { 
+            once: true
+          });
+        }
+      }
+    },
     template:
     `
     <v-card>
       <v-card-title primary-title>
         <div>
-          <div class="moreOption">...</div>
           <h3 class="headline mb-0">{{event.name}}</h3>
           <div>{{event.asso}}</div>
-          <ul class="moreOptionMenu">
+          <div class="moreOption"  v-on:click="openOption">...</div>
+          <ul class="moreOptionMenu" style="display:none">
             <li class="followEvtOpt">Suivre l'événement</li>
             <li class="HideEvtOpt">Masquer l'événement</li>
             <li class="HideEvtAssoOpt">Masquer les événements de {{event.asso}}</li>
