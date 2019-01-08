a9a269ee5635f24c77a9a5d3a60669be000e1c59
Antonin Vianey <Antonin.Vianey@live.com>
2019-01-08 20:50:20 +0100

    Signed-off-by: Antonin Vianey <Antonin.Vianey@live.com>

--- IHM/JS/components/event-feed.component.js
+++ IHM/JS/components/event-feed.component.js
@@ -1,12 +1,63 @@
 Vue.component('feed-event', {
     props: ['event'],
+    methods: {
+      openOption (event)
+      {
+        event.stopPropagation();
+
+        var vue = this;
+
+        var moreOptionBtn = event.target;
+        var moreOptionMenu = moreOptionBtn.nextElementSibling;
+
+        if ( moreOptionMenu.style.display == "block")
+        {
+          moreOptionMenu.style.display = "none";
+        }
+        else
+        {
+
+          document.body.addEventListener("click", function()
+          {
+
+             moreOptionMenu.style.display="none";
+         }, 
+         {
+            one: true
+         });
+
+          var moreOptionMenus = document.getElementsByClassName('moreOptionMenu');
+          for (var i = 0; i < moreOptionMenus.length; i++)
+          {
+            var moreOptionMenu2 = moreOptionMenus[i];
+            moreOptionMenu2.style.display="none";
+          }   
+
+          moreOptionMenu.style.display = "block";
+
+          moreOptionMenu.addEventListener("click", function(event)
+          {
+            event.stopPropagation();
+          });
+        }
+      }
+    },
     template:
     `
     <v-card>
       <v-card-title primary-title>
-        <div>
+        <div style="width:100%">
           <h3 class="headline mb-0">{{event.name}}</h3>
           <div>{{event.asso}}</div>
+          <div class="moreOption"  v-on:click="openOption">...</div>
+          <ul class="moreOptionMenu" style="display:none">
+            <hr />
+            <li class="followEvtOpt">Suivre l'événement</li>
+            <li class="HideEvtOpt">Masquer l'événement</li>
+            <li class="HideEvtAssoOpt">Masquer les événements de {{event.asso}}</li>
+            <li class="ShowDetEvt">Plus de détails</li>
+            <li class="ContAsso">Contacter l'organsiation</li>
+          </ul>
         </div>
       </v-card-title>
       <v-responsive>
